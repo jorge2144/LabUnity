0496bdb325656e6930b1091beda60464fbb67f45
Juan Diego Vizcardo Chavez <juan_vizcardo@usmp.pe>
2020-10-20 23:37:03 -0500

    vista y movimiento primera persona

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
